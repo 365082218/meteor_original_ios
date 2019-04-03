393b8a020d5b0f53cc2b647608fee8b293adda3a
365082218@qq.com <365082218@qq.com>
2019-04-04 03:07:59 +0800

    ++

--- Assets/Meteor/AudioButton.cs
+++ Assets/Meteor/AudioButton.cs
@@ -7,11 +7,11 @@ using UnityEngine.UI;
 
 public class AudioButton : Button
 {
-    [SerializeField] private AudioAsset audio = null;
+    [SerializeField] private AudioAsset audioS = null;
     public override void OnPointerClick(PointerEventData eventData)
     {
-        if (audio != null)
-            SoundManager.Instance.PlaySound(audio.clipName);
+        if (audioS != null)
+            SoundManager.Instance.PlaySound(audioS.clipName);
         base.OnPointerClick(eventData);
     }
 }
